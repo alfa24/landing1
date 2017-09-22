e26e362eca4f53c8abbd9dca288e618b1006433c
Falichev Aleksandr <6064233@gmail.com>
2017-09-23 00:03:16 +0800

    init

---
+++ includes/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+
+    $('#sendContact').click(function (e) {
+        e.preventDefault();
+        var formValid = true;
+        var errMessage = '';
+
+        function valid(element) {
+            var formGroup = $(element).parents('.form-group');
+            if (element.checkValidity()) {
+                formGroup.addClass('has-success').removeClass('has-error');
+            } else {
+                formGroup.addClass('has-error').removeClass('has-success');
+                if (formValid) {
+                    errMessage = element.validationMessage;
+                }
+                formValid = false;
+            }
+        };
+
+        //перебрать все элементы управления input
+        $('input').each(function () {
+            valid(this);
+        });
+
+        inputMessage = $("#message");
+        if (inputMessage.val().length < 5) {
+            if (formValid) {
+                errMessage = 'Too short message! Please enter something.';
+            }
+            formValid = false;
+            $(inputMessage).parents('.form-group').addClass('has-error').removeClass('has-success');
+        } else {
+            $(inputMessage).parents('.form-group').addClass('has-success').removeClass('has-error');
+        }
+
+        //если форма валидна, то
+        if (formValid) {
+            $('#errorAlert').slideUp();
+            $('#successAlert').slideDown();
+            $('#modal-1').modal('show');
+        } else {
+            $('#errorMsg').text(errMessage);
+            $('#successAlert').slideUp();
+            $('#errorAlert').slideDown();
+        }
+    });
+
+    $('#closeAlert').click(function (e) {
+        e.preventDefault();
+        $('#successAlert').slideUp();
+    });
+
+    $('#closeErrorAlert').click(function (e) {
+        e.preventDefault();
+        $('#errorAlert').slideUp();
+    });
+
+
+})
