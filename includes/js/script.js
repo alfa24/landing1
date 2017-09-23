dfbe29ed6d41f33330b7890ce84972037295e181
Falichev Aleksandr <6064233@gmail.com>
2017-09-23 12:55:17 +0800

    add scroll. fix bags.

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    //send contact
     $('#sendContact').click(function (e) {
         e.preventDefault();
         var formValid = true;
@@ -55,12 +57,29 @@ $(document).ready(function () {
         $('#errorAlert').slideUp();
     });
 
-    $(' .parallax-window ').parallax({
-        // imageSrc :  'images/bg.png',
+
+    //scroll
+    $(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+    });
+
+    //parallax
+    $('.parallax-window-header').parallax({
         naturalWidth: 1400,
         naturalHeight: 800,
-        speed: 0.2,
-        bleed: 10
+        speed: 0.2
     });
+
+    $('.parallax-window-map').parallax({
+        naturalWidth: 1553,
+        naturalHeight: 609,
+        speed: 0.2,
+        bleed: 200
+    });
+
     jQuery(window).trigger('resize').trigger('scroll');
 });
